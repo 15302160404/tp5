53b62c423dcaa4eaeba95417ddf5cd780425d0d6
15302160404 <15767314719@189.cn>
2018-03-22 11:56:00 +0800

    初始化
    引入H-ui前端框架

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -2,18 +2,14 @@
 namespace app\index\controller;
 //use think\Db;//引入数据类
 use app\common\model\Teacher;
-class TeacherController{
+use think\Controller;
+class TeacherController extends Controller{
 	public function index()
 	{
-  
-		
-
-		$teachers=Teacher::all(['sex'=>1,]);
-		
-	    dump($teachers);
+		return $this->fetch();
 	}
 	public function hi()
 	{
-		echo "aaa";
+		echo "hi";
 	}
 }
