6baf0c748462354422455662d018951d44905cb7
15302160404 <15767314719@189.cn>
2018-04-28 11:43:19 +0800

    course_yzm

--- application/common/validate/Course.php
+++ application/common/validate/Course.php
@@ -6,8 +6,11 @@ class Course extends Validate
 {
     protected $rule = [
         'name' => 'require',
+        'vote' =>'require|captcha'
     ];
     protected $message = [
         'name.require'=>'课程名字不能为空',
+        'vote.require'=>'验证码不能为空',
+        'vote.captcha'=>'验证码不正确'
     ];
 }
